89679f948d3916e590123ac0c672f856d68da609
s-fueldao <s@fueldao.io>
2024-06-17 14:30:20 +0000

    chore: register dapp canisters with NNS root

---
+++ proposal/2024/1-register-dapp-with-nns.sh
@@ -0,0 +1,4 @@
+dfx canister --network ic update-settings --add-controller r7inp-6aaaa-aaaaa-aaabq-cai 67erj-tiaaa-aaaam-acnxa-cai
+dfx canister --network ic update-settings --add-controller r7inp-6aaaa-aaaaa-aaabq-cai 6kdae-saaaa-aaaam-acnuq-cai
+dfx canister --network ic update-settings --add-controller r7inp-6aaaa-aaaaa-aaabq-cai 6ncgq-7yaaa-aaaam-acnua-cai
+dfx canister --network ic update-settings --add-controller r7inp-6aaaa-aaaaa-aaabq-cai 7amc6-qqaaa-aaaam-acntq-cai
