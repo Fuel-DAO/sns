1c457241779638daa8e4e6a32503baf0235ec333
s-fueldao <s@fueldao.io>
2025-01-25 14:02:14 +0000

    chore: query neruon state

--- proposal/2025/4-check-neuron-state.sh
+++ proposal/2025/4-check-neuron-state.sh
@@ -6,10 +6,12 @@ dfx identity use "${IDENTITY_NAME}"
 # 14199361421955379993
 
 ./quill \
-  get-neuron-info \
-  --pem-file "${PEM_FILE}" \
-  14199361421955379993 --yes
-  
-#   > ./proposal/2024/4-check-neuron-state.json
+  list-neurons \
+  --pem-file "${PEM_FILE}" > ./proposal/2025/4-check-neuron-state.json
 
-# ./quill send --yes ./proposal/2024/4-check-neuron-state.json
+./quill send --yes ./proposal/2025/4-check-neuron-state.json
+
+# ./quill \
+#   get-neuron-info \
+#   --pem-file "${PEM_FILE}" \
+#   14199361421955379993 --yes
